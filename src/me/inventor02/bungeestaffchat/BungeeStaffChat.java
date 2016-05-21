fbd11322d9f20668209bed6fef6449ab5b12793a
George Peppard <iaminventor02@gmail.com>
2016-05-21 14:29:21 +0100

    Base for a plugin

---
+++ src/me/inventor02/bungeestaffchat/BungeeStaffChat.java
@@ -0,0 +1,15 @@
+package me.inventor02.bungeestaffchat;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeStaffChat extends Plugin {
+    @Override
+    public void onEnable() {
+        //
+    }
+
+    @Override
+    public void onDisable() {
+        //
+    }
+}
---
+++ src/me/inventor02/bungeestaffchat/commands/StaffChatCommand.java
@@ -0,0 +1,15 @@
+package me.inventor02.bungeestaffchat.commands;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class StaffChatCommand extends Command {
+    public StaffChatCommand() {
+        super("s");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        //
+    }
+}
